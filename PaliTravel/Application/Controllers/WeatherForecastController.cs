1dbb0d75991c347cdad3faec2b11c5ceebe00524
Musab Abuasi <abuasimusab@gmail.com>
2025-01-13 17:56:52 +0200

    Testing jwt on dummy weather api

--- PaliTravel/Application/Controllers/WeatherForecastController.cs
+++ PaliTravel/Application/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PaliTravel.Application.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Authorize]
+[Route("api/weather")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -18,15 +20,9 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    // [HttpGet(Name = "GetWeatherForecast")]
-    // public IEnumerable<WeatherForecast> Get()
-    // {
-    //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    //         {
-    //             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-    //             TemperatureC = Random.Shared.Next(-20, 55),
-    //             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    //         })
-    //         .ToArray();
-    // }
+    [HttpGet("woo")]
+    public IActionResult Get()
+    {
+        return Ok("woo");
+    }
 }
